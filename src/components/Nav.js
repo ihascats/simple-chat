7a96d31e1eba8c2f298db92b3fe509a6d583889c
ihascats <s.pavlovic.rs@gmail.com>
2022-06-30 16:10:35 +0200

    Style sign in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -18,7 +18,7 @@ export default function Nav() {
     });
 
     setDisplayInfo(
-      <div>
+      <div className="user">
         <p>{name}</p>
         <img src={userPicture} alt="user image" />
       </div>,
@@ -26,15 +26,23 @@ export default function Nav() {
   };
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          processInformation(signInWithGoogle());
-        }}
-      >
-        SIGN IN WITH GOOGLE
-      </button>
-      {displayInfo}
-    </div>
+    <nav>
+      {displayInfo ? (
+        displayInfo
+      ) : (
+        <button
+          onClick={() => {
+            processInformation(signInWithGoogle());
+          }}
+          className="signIn"
+        >
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/800px-Google_%22G%22_Logo.svg.png"
+            alt="google logo"
+          />
+          Sign In
+        </button>
+      )}
+    </nav>
   );
 }
